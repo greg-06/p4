3dd251adedc9b66a9e6bd8a70270ec093fad2005
Grégory Sibemilia <Gregory06@live.fr>
2021-09-07 11:32:45 +0200

    ajout classe Manager + docstring

---
+++ manager.py
@@ -0,0 +1,8 @@
+class Manager:
+    """on gère une collection d'entités chargés à partir d'une source de données (json):
+        charger un fichier json
+        créer une entité (joueur ou tournoi)
+        pouvoir retrouver une entité via son identifiant
+        pouvoir récupérer la liste des entités
+    """
+    pass
