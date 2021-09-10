b74e33ac3c06d72d853c53d975cfb6f539ef244a
Grégory Sibemilia <Gregory06@live.fr>
2021-09-10 15:18:26 +0200

    définition des docstrings

--- models/player.py
+++ models/player.py
@@ -7,7 +7,7 @@ from utilities.fonctions import from_birthdate_to_age
 
 
 class Gender(str, Enum):
-    """création d'une classe 'sexe' qui a pour attributs homme ("M") ou femme ("F")"""
+    """création d'une type de variable qui ne peut avoir que 2 valeurs (M ou F)"""
     male = "M"
     female = "F"
 
@@ -26,20 +26,20 @@ class Player(BaseModel):
 
     @validator("first_name", "last_name")
     def check_name(cls, v: str):
-        """cette fonction permet au validateur de s'assurer que les paramètres du prénom et du nom contiennent uniquement les caractères passés dans notre regex
+        """validateur qui s'assurer que les paramètres du prénom et du nom contiennent uniquement les caractères passés dans notre regex
             si les caractères ne correspondent pas
                 renvoie une erreur
-            on renvoie le validateur avec une majuscule en début de phrase"""
+            on renvoie la valeur avec une majuscule en début de phrase"""
         if not re.match(r"^[A-Za-z \-'çéèàâêîôûäëïöü]{2,18}$", v):
             raise ValueError("Votre nom n'est pas valide")
         return v.title()
 
     @validator("birthdate")
     def check_age(cls, v: date):
-        """fonction qui permet au validateur 'birthdate' de s'assurer que la date de naissance correspond aux critères de validation
+        """validateur qui s'assure que la date de naissance correspond aux critères de validation
             si la date de naissance est inférieur à 18 ans
                 renvoie une erreur
-        on renvoie le validateur"""
+        on renvoie la valeur"""
         if from_birthdate_to_age(v) < 18:
             raise ValueError("Vous n'avez pas l'âge légal pour vous inscrire")
         return v
