db953ebf6ac2325d8e8611cfe126fd80e00dae33
Grégory Sibemilia <Gregory06@live.fr>
2021-09-10 11:04:02 +0200

    construction classe Manager avec le model "player"

--- manager.py
+++ manager.py
@@ -1,8 +1,22 @@
+import json
+from models.player import Player
+
+
 class Manager:
     """on gère une collection d'entités chargés à partir d'une source de données (json):
-        charger un fichier json
-        créer une entité (joueur ou tournoi)
-        pouvoir retrouver une entité via son identifiant
-        pouvoir récupérer la liste des entités
-    """
-    pass
+    charger un fichier json
+    créer une entité (joueur ou tournoi)
+    pouvoir retrouver une entité via son identifiant
+    pouvoir récupérer la liste des entités"""
+    def __init__(self) -> None:
+        """attributs 
+        d'instance 
+        de class"""         
+        pass
+    
+    def manage_players(self):
+        with open("players.json", "w") as write_file:
+            json.dump("players.json", write_file)
+
+      
+    
