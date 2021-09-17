8859359ad6305ac22d6ac83945c027e2eecfa4ed
Grégory Sibemilia <Gregory06@live.fr>
2021-09-17 17:42:57 +0200

    code du fichier main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from managers import tournament_manager, player_manager
+
+
+def main():
+    """"""
+    tournament_manager.load_json_from_file("JSON/tournaments.json")
+    player_manager.load_json_from_file("JSON/players.json")
+    
+if __name__ == "__main__":
+    main()
