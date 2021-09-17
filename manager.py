e9d0df3282376dcd4dd41719ba44daa8b60371b0
Grégory Sibemilia <Gregory06@live.fr>
2021-09-17 15:26:47 +0200

    finalisation

--- manager.py
+++ manager.py
@@ -1,8 +1,7 @@
 import json
 from typing import Any
 from pydantic.types import PositiveInt
-from models.player import Player
-from models.tournament import Tournament
+
 
 """le fichier manager est destiné à implémenter
 le crud = create read update delete
@@ -12,7 +11,7 @@ le crud = create read update delete
 avec ses tours et matchs """
 
 
-class Manage_player:
+class Manager:
     """on gère une collection d'entités chargés
     à partir d'une source de données (json):
     charger un fichier json
@@ -39,36 +38,3 @@ class Manage_player:
         item = self.items_type(**data)  # ** opérateur de décompactage nommé
         self.items[item.id] = item
         return item
-
-
-class Manage_tournoi:
-    def __init__(self, items_type: Any) -> None:
-        self.items = {}
-        self.items_type = items_type
-
-    def read(self):
-        return list(self.items.values())
-
-    def read_by_id(self, id: PositiveInt):
-        return self.items[id]
-
-    def load_json_from_file(self, path: str):
-        with open(path) as read_file:
-            json_data = json.load(read_file)
-            for data in json_data:
-                self.create(data)
-
-    def create(self, data):
-        item = self.items_type(**data)  # ** opérateur de décompactage nommé
-        self.items[item.id] = item
-        return item
-    
-
-player_list_json = Manage_player(Player)
-player_list_json.load_json_from_file("JSON/players.json")
-print(player_list_json.items)
-
-tournament_list_json = Manage_tournoi(Tournament)
-tournament_list_json.load_json_from_file("JSON/tournaments.json")
-print(tournament_list_json)
-
