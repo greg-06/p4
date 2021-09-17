f0527c214f4c3b5ae074cc02db841117c4f9829a
Grégory Sibemilia <Gregory06@live.fr>
2021-09-17 17:38:02 +0200

    création du fichier managers

---
+++ managers.py
@@ -0,0 +1,6 @@
+from utilities.manager import Manager
+from models.player import Player
+from models.tournament import Tournament
+
+tournament_manager = Manager(Tournament)
+player_manager = Manager(Player)
