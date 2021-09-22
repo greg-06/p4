1496569bdabc302b0549dc100aedbefea4c762f0
Grégory Sibemilia <Gregory06@live.fr>
2021-09-22 16:25:11 +0200

    pep8 correction

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def main():
     """"""
     tournament_manager.load_json_from_file("JSON/tournaments.json")
     player_manager.load_json_from_file("JSON/players.json")
-    
+
+
 if __name__ == "__main__":
     main()
