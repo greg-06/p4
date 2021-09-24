9209dccffe2fac4c4c78be48aea82fbd4b14620f
Grégory Sibemilia <Gregory06@live.fr>
2021-09-24 10:52:30 +0200

    Création de la classe Menu

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+
 class View:
     """affiche un titre et un message"""
     def __init__(self, title, message):
@@ -5,16 +6,47 @@ class View:
         self.message = message
 
     def display(self):
-        print("=====" + "\n" + self.title + "\n" + "=====" + "\n" + self.message)
+        print("=" * 50 + "\n" + self.title + "\n" + "=" * 50 + "\n" + self.message + "\n" + "_" * 50)
+
+
+# View("Titre", "Message").display()
 
 
 class Menu(View):
-    def __init__(self):
-        super().__init__(self)
-        self.view = "view"
+    def __init__(self, title, message, menu):
+        super().__init__(title, message)
+        self.menu = menu
+
+    def display(self):
+        print("Titre : " + self.title)
+        print("Message : " + self.message)
+        print("Menu : " + self.menu)
 
 
-View("Titre", "Message").display()
+view = View("mon_titre", "mon_message")
+view.display()
 
-""" créer une classe menu qui hérite de la classe view. Rajouter l'input.
+menu1 = Menu("titre_test", "message_test", "menu_test")
+menu1.display()
+
+
+# MAIN_MENU_CHOICES = ["1", "2", "3"]
+
+# TEXTE_MAIN_MENU = """1 Manage tournaments
+# 2 Manage players
+# 3 Quit the program"""
+
+# View("Main menu", TEXTE_MAIN_MENU).display()
+
+""" créer une classe menu qui hérite de la classe view.
 Héritage et métaclasses à voir, iso 8601 à voir également. """
+
+# while MAIN_MENU_CHOICES != 3:
+#     user_choice = ""
+#     if user_choice == "1":
+#         print("")
+#     if user_choice == "1":
+#         print("")
+#     elif user_choice == "3":
+#         sys.exit()
+#     print("Thanks for using this program. Goodbye !")
