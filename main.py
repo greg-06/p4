1fb5aeb2cc84e24254702fdb8e655dcb44c27b85
Grégory Sibemilia <Gregory06@live.fr>
2021-10-01 16:24:12 +0200

    docstring

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from managers import tournament_manager, player_manager
 
 
-def main():
-    """"""
+def main():  # le main est le controleur
+    """afficher un menu principal
+    permettre à l'utilisateur de faire sa selecion
+    et le rediriger vers le sous-menu (joeur, tournoi ou quitter)"""
     tournament_manager.load_json_from_file("JSON/tournaments.json")
     player_manager.load_json_from_file("JSON/players.json")
 
