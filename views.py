bcdcdabf690e486958639372ca1e816eb50fb2b8
Grégory Sibemilia <Gregory06@live.fr>
2021-10-01 16:24:47 +0200

    finalisation classe Form

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 
-from typing import List
-from models.player import Player
+from typing import Dict, List
+
 
 # Classe mère
 class View:
@@ -9,7 +9,7 @@ class View:
         self.message = message
 
     def display(self):
-        print("=" * 50 + "\n" + self.title + "\n" + "=" * 50 + "\n" + self.message + "\n" + "_" * 50 + "\n")
+        print("=" * 50 + "\n" + self.title + "\n" + "=" * 50 + "\n" + self.message + "\n" + "=" * 50)
 
 
 # View("Titre", "Message").display()
@@ -34,11 +34,6 @@ class Menu(View):
                 pass
 
 
-''' créer une classe <formulaire> héritant de View,
-    capable de demander à l'utilisateur de rentrer des champs
-    et l'ensemble de ces champs sont retournés par display en tant que dictionnaire'''
-
-
 PLAYER_DICT = """ID : <nombre entier>
 Classement : <entre 1000 et 2000>
 Nom : <nom>
@@ -46,64 +41,38 @@ Prénom : <prénom>
 Date de naissance : <YYYY-MM-DD>
 Sexe : <M> ou <F>"""
 
-player_dict_model = {
-        "id": "",
-        "rank": "",
-        "first_name": "",
-        "last_name": "",
-        "birthdate": "",
-        "gender": ""
-    }
-
-nb_champs = len(player_dict_model)
-
-
-# for i in list(player_dict_model):
-#     choice = input("Saisissez l'id du joueur : ")
-#     player_dict_model["id"] = choice    
-#     choice = input("Saisissez le classement du joueur : ")
-#     player_dict_model["rank"] = choice
-#     choice = input("Saisissez le nom du joueur : ")
-#     player_dict_model["first_name"] = choice
-#     choice = input("Saisissez le prénom du joueur : ")
-#     player_dict_model["last_name"] = choice
-#     choice = input("date de naissance : ")
-#     player_dict_model["birthdate"] = choice
-#     choice = input("Sexe : ")
-#     player_dict_model["gender"] = choice                    
-#     print(player_dict_model)
-#     break
-
 
 class Form(View):
-    def __init_(self, title: str, message: str):
+    def __init__(self, title: str, message: str, fields: Dict[str, str]):
         View.__init__(self, title, message)
+        self.fields = fields
 
     def display(self):
+        data = {}
         super().display()
-        while True:
-            try:
-                if player_dict_model:                    
-                    choice = input("Saisissez l'id du joueur : ")
-                    player_dict_model["id"] = choice
-                    choice = input("Saisissez le classement du joueur : ")
-                    player_dict_model["rank"] = choice
-                    choice = input("Saisissez le nom du joueur : ")
-                    player_dict_model["first_name"] = choice
-                    choice = input("Saisissez le prénom du joueur : ")
-                    player_dict_model["last_name"] = choice
-                    choice = input("date de naissance : ")
-                    player_dict_model["birthdate"] = choice
-                    choice = input("Sexe : ")
-                    player_dict_model["gender"] = choice
-                    print(player_dict_model)
-                    return choice
-            except ValueError:
-                pass
+        for field_name, field_desc in self.fields.items():
+            data[field_name] = input(field_desc + " : ")
+        return data
 
 
-formulaire = Form("Fiche joueur", str(PLAYER_DICT))
-formulaire.display()
+class PlayerAddForm(Form):
+    def __init__(self):
+        super().__init__(title="Fiche joueur", message=PLAYER_DICT, fields={
+            "id": "Id du joueur",
+            "rank": "Classement du joueur",
+            "first_name": "Nom du joueur",
+            "last_name": "Prénom du joueur",
+            "birthdate": "Date de naissance",
+            "gender": "Sexe"
+        })
+
+
+test = PlayerAddForm()
+print(test.display())
+
+
+# formulaire = Form("Fiche joueur", str(PLAYER_DICT))
+# formulaire.display()
 
 # main_menu = Menu("Main menu", ["Manage tournaments", "Manage players", "Quit the program"])
 # main_menu.display()
