c149b5a72688866fe9b6f484b316345e587894e7
Grégory Sibemilia <Gregory06@live.fr>
2021-10-09 18:41:44 +0200

    ajout de vues supplémentaires (class)

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 
 from typing import Dict, List
+import os
 
 
 # Classe mère
@@ -9,24 +10,25 @@ class View:
         self.message = message
 
     def display(self):
+        os.system("cls")  # TD : rendre compatible unix
         print("=" * 50 + "\n" + self.title + "\n" + "=" * 50 + "\n" + self.message + "\n" + "=" * 50)
 
-
-# View("Titre", "Message").display()
 
 # Classe fille
 class Menu(View):
     def __init__(self, title: str, options: List[str]):
+        """"""
         message = "\n".join([f"{nb}: {option}" for nb, option in enumerate(options, start=1)])
         View.__init__(self, title, message)
         self.options = options
 
     def display(self):
+        """"""
         super().display()
         while True:
             try:
-                choice = int(input("Choisissez votre option : "))
-                if 0 < choice < len(self.options):
+                choice = int(input(f"👉  Choisissez votre option entre 1 et {len(self.options)} :  "))
+                if 0 < choice <= len(self.options):
                     return choice
                 else:
                     print("Entrez une option valide")
@@ -42,12 +44,23 @@ Date de naissance : <YYYY-MM-DD>
 Sexe : <M> ou <F>"""
 
 
+TOURNAMENT_DICT = """ID : <nombre entier>,
+Place: <ville>
+Date_start: <YYYY-MM-DD>,
+Date_end: <YYYY-MM-DD>,
+Nb_turns (4 par défaut): <nombre entier>,
+Players: <ID des joueurs>,
+Turns: <liste des tours>"""
+
+
 class Form(View):
+    """"""
     def __init__(self, title: str, message: str, fields: Dict[str, str]):
         View.__init__(self, title, message)
         self.fields = fields
 
     def display(self):
+        """"""
         data = {}
         super().display()
         for field_name, field_desc in self.fields.items():
@@ -56,29 +69,80 @@ class Form(View):
 
 
 class PlayerAddForm(Form):
+    """"""
     def __init__(self):
+        """"""
         super().__init__(title="Fiche joueur", message=PLAYER_DICT, fields={
-            "id": "Id du joueur",
-            "rank": "Classement du joueur",
             "first_name": "Nom du joueur",
             "last_name": "Prénom du joueur",
-            "birthdate": "Date de naissance",
-            "gender": "Sexe"
+            "birthdate_year": "Année de naissance du joueur : ",
+            "birthdate_month": "Moi de naissance du joueur : ",
+            "birthdate_day": "Jour de naissance du joueur : ",
+            "gender": "Sexe",
+            "rank": "Classement du joueur"
         })
 
 
-test = PlayerAddForm()
-print(test.display())
+class TournamentAddForm(Form):
+    def __init__(self):
+        """"""
+        super().__init__(title="Fiche tournoi", message=TOURNAMENT_DICT, fields={
+            "name": "Nom du tournoi",
+            "place": "Lieu du tournoi",
+            "nb_turns": "Nombre tours",
+            "nb_players": "Nombre de participants"
+        })
 
 
-# formulaire = Form("Fiche joueur", str(PLAYER_DICT))
-# formulaire.display()
+class ManagePlayerMenu(Menu):
+    def __init__(self):
+        """"""
+        super().__init__("🧾 Manage players menu", ["New player", "List players by name", "List players by rank", "Edit player rank", "Back"])
 
-# main_menu = Menu("Main menu", ["Manage tournaments", "Manage players", "Quit the program"])
-# main_menu.display()
 
-# tournament_menu = Menu("Tournament Menu", ["Create", "List", "Resume1", "Back to main menu"])
-# tournament_menu.display()
+class MainMenu(Menu):
+    def __init__(self):
+        """"""
+        super().__init__("📌 Menu principal", ["Manage tournaments", "Manage players", "Quit the program"])
 
-# players_menu = Menu("Players Menu", ["By name", "By rank", "Player rank", "Back to Main menu"])
-# players_menu.display()
+
+class ManageTournament(Menu):
+    def __init__(self):
+        """"""
+        super().__init__("🧾 Manage tournaments menu", ["Create tournament", "List tournaments", "Resume tournament", "Back"])
+
+
+class CreateTournamentMenu(Menu):
+    def __init__(self):
+        """"""
+        super().__init__("🧾 Create tournament", ["Name", "Place", "Date", "Tours Number", "Description", "Time Control", "Back"])
+
+
+class ListTournamentsMenu(Menu):
+    def __init__(self):
+        super().__init__("🧾 List tournaments", ["Tournament1", "Tournament2", "Tournament3", "Back"])
+
+
+class ResumeTournamentMenu(Menu):
+    def __init__(self):
+        super().__init__("🧾 Resume menu", ["Resume", "Back"])
+
+
+class NewPlayerMenu(Menu):
+    def __init__(self):
+        super().__init__("🧾 New Player menu", ["First Name", "Last name", "Birthdate_year", "Birthdate_month", "Birthdate_day", "Gender", "Rank", "Back"])
+
+
+class ListPlayersByNameMenu(Menu):
+    def __init__(self):
+        super().__init__("🧾 List player by name menu", ["player1", "player2", "player3", "player4", "player5", "player6", "player7", "player8", "back"])
+
+
+class ListPlayersByRankMenu(Menu):
+    def __init__(self):
+        super().__init__("🧾 List player by rank menu", ["player1 rank", "player2 rank", "player3 rank", "player4 rank", "player5 rank", "player6 rank", "player7 rank", "player8 rank", "back"])
+
+
+class EditPlayerRankMenu(Menu):
+    def __init__(self):
+        super().__init__("🧾 Edit player rank menu", ["Edit player_01 rank", "Edit player_02 rank", "Edit player_03 rank", "Edit player_04 rank", "Edit player_05 rank", "Edit player_06 rank", "Edit player_07 rank", "Edit player_08 rank", "back"])
