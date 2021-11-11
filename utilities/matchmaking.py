8463503f21199196cdd1097f8c6d938c10ab61b8
Grégory Sibemilia <Gregory06@live.fr>
2021-11-11 19:23:29 +0100

    change name object for more explicite name

--- utilities/matchmaking.py
+++ utilities/matchmaking.py
@@ -1,23 +1,23 @@
 
 
-def gen_matchup(p1, p2):
+def gen_matchup(player1, player2):
     """ Génèration d'une rencontre
     retourne une paire ordonnée d'identifiants des deux joueurs du match"""
-    return tuple(sorted((p1['id'], p2['id'])))
+    return tuple(sorted((player1['id'], player2['id'])))
 
 
-def get_player_score(p_id, scores):
+def get_player_score(player_id, scores):
     '''Lecture du score d'un joueur
         on recherche un joueur dans la liste des scores
         et si on le trouve on incrémente son score
     on retourne le score total'''
-    s = 0
-    for (p1_id, s1), (p2_id, s2) in scores:
-        if p_id == p1_id:
-            s += s1
-        elif p_id == p2_id:
-            s += s2
-    return s
+    score = 0
+    for (player1_id, score1), (player2_id, score2) in scores:
+        if player_id == player1_id:
+            score += score1
+        elif player_id == player2_id:
+            score += score2
+    return score
 
 
 def gen_round(round, players, tournament_matchups, tournament_scores):
@@ -43,17 +43,17 @@ def gen_round(round, players, tournament_matchups, tournament_scores):
     if round == 1:
         players = sorted(players, key=lambda x: -x["rank"])
         matchups = [
-            gen_matchup(p1, p2) for p1, p2 in zip(players[:4], players[4:])]
+            gen_matchup(player1, player2) for player1, player2 in zip(players[:4], players[4:])]
         tournament_matchups += matchups
     else:
         round_players = sorted(
             players, key=lambda x: (
                 -get_player_score(x["id"], tournament_scores), -x["rank"]))
         while round_players:
-            p1 = round_players.pop(0)
-            p2 = search_opponent(
-                p1, round_players, tournament_matchups)
-            match = gen_matchup(p1, p2)
+            player1 = round_players.pop(0)
+            player2 = search_opponent(
+                player1, round_players, tournament_matchups)
+            match = gen_matchup(player1, player2)
             matchups.append(match)
             tournament_matchups.append(match)
     print(matchups)
@@ -65,18 +65,18 @@ def gen_score(matchup):
        le score du joueur 1 est demandé à l'utilisateur
        le score du joueur 2 est déduit du score du joueur 1
     retourne un match joué sous la forme d'un tuple de deux tuples'''
-    s1 = float(input("Entrez un score: "))
-    s2 = 1 - s1
-    return ((matchup[0], s1), ((matchup[1], s2)))
+    score1 = float(input("Entrez un score: "))
+    score2 = 1 - score1
+    return ((matchup[0], score1), ((matchup[1], score2)))
 
 
-def search_opponent(p1, players, played_matchups):
+def search_opponent(player1, players, played_matchups):
     """Recherche d'un adversaire dans les joueurs restant du tour
     si le match a déjà été joué, on prend le joueur suivant
     si tous les matchs ont déjà été joués
     on prend le joueur ayant le meilleur score"""
     for p2 in players:
-        matchup = gen_matchup(p1, p2)
+        matchup = gen_matchup(player1, p2)
         if matchup not in played_matchups:
             players.pop(players.index(p2))
             return p2
